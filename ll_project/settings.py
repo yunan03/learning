dd2eb013f706329010ea0bbea6ac89d35bbe0a83
yunan <yunan031019@126.com>
2024-06-27 15:54:13 +0800

    ALLOWED HOST 设置

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cn2waoy&dn-x^jyu9d4ly&zko9*v1#jg+2$pl!#mfr^(f34*))'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
